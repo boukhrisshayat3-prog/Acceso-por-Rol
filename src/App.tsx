ab20b7c8f274c907e739836f0b9571ebde854c60
boukhrisshayat3-prog <boukhrisshayat3@alumnos.cei.es>
2026-03-11 21:34:10 +0100

    Refactor App component to enhance user role management and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,18 @@ import ProtectedRoute from './components/AccesoPorRol/AccesoPorRol'
 
 //pagina de ejemplo
 const Home = () => <h1 className='text-1xl'>Home</h1>
-const Login = () => <h1 className='text-1xl text-indigo-600 font-bold'>Panel de Administracion</h1>
+const Admin= () => <h1 className='text-1xl text-green-600 font-bold'>Bienvenido Admin</h1>
+const Login = () => <h1 className='text-1xl text-indigo-600 font-bold'>Iniciar Sesion</h1>
 const AccessDenied = () => <h1 className='text-1xl text-red-600'>Acceso Denegado</h1>
+//paginas protegidas por rol
+const Perfil = () => <h1 className='text-1xl text-blue-600 font-bold'>Perfil de Usuario</h1>
+const DashboardAdmin = () => <h1 className='text-1xl text-green-600 font-bold'>Dashboard Admin(solo para admin)</h1>
 
 function App() {
   const [user, setUser] = useState<{ id: number; name: string; role: 'admin' | 'user' } | null>(null)
-  const login = () => setUser ({ id: 1, name: 'mami', role: 'user' })
+  //funciones simuladores
+  const loginAdmin = () => setUser ({ id: 1, name: 'mami', role: 'admin' })
+  const loginUser = () => setUser ({ id: 1, name: 'mami', role: 'user' })
   const logout = () => setUser(null)
 
 
@@ -25,10 +31,22 @@ function App() {
         <div className='flex gap-4'>
           <Link to="/">Inicio</Link>
           <Link to="/admin">Admin (VIP)</Link>
+          <Link to="/perfil">Perfil</Link> 
+          
         </div>
-       <button className={`px-3 py-1 rounded ${user ? 'bg-red-500': 'bg-green-500'}`} onClick={user ? logout : login}>
-      {user ? 'Cerrar Sesion': 'Simular Login'}
-    </button>
+        //panel de controle
+        <div className='flex gap-2 item-center'>
+          <span>{user ? `Hola, ${user.name} (${user.role})` : 'Desconnectado'}</span>
+          <button className={`px-3 py-1 rounded ${user?.role === 'user' ? 'bg-green-500': 'bg-blue-500'}`} onClick={loginUser}>
+           entrar como user
+          </button>
+           <button className={`px-3 py-1 rounded ${user?.role === 'admin' ? 'bg-green-500': 'bg-blue-500'}`} onClick={loginAdmin}>
+            entrar como admin
+          </button>
+          <button className={`px-3 py-1 rounded ${user ? 'bg-red-500': 'bg-green-500'}`} onClick={logout}>
+            Cerrar Sesion
+          </button>
+        </div>
     </nav>
     <div className="p-10">
     <Routes>
